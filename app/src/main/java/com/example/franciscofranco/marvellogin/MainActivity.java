e5604fe0432c5956955216b5816b52e18b8da8b6
Franco <frfranco@uci.edu>
2016-10-09 18:52:58 -0700

    succesfully added login and network error photos for landscape and portrait

--- app/src/main/java/com/example/franciscofranco/marvellogin/MainActivity.java
+++ app/src/main/java/com/example/franciscofranco/marvellogin/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.franciscofranco.marvellogin;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
@@ -51,15 +52,36 @@ public class MainActivity extends AppCompatActivity
     }
 
     private void displayLogin() {
-        Picasso.with(this)
-                .load(R.mipmap.login)
-                .fit()
-                .into(image);
+        int value = 0;
+
+        if(getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE){
+            value = R.mipmap.login_landscape;
+            Picasso.with(this)
+                    .load(value)
+                    .fit()
+                    .into(image);
+        } else {
+            value = R.mipmap.login;
+            Picasso.with(this)
+                    .load(value)
+                    .fit()
+                    .into(image);
+        }
+
+
     }
 
     private void displayNetworkError() {
+        int value = 0;
+
+        if(getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE){
+            value = R.mipmap.network_error_landscape;
+        } else {
+            value = R.mipmap.network_error_portrait;
+        }
+
         Picasso.with(this)
-                .load(R.mipmap.network_error_portrait)
+                .load(value)
                 .fit()
                 .into(image);
     }
