d828fc9947f254f56d91b71e8842a90de509a6c0
Franco <frfranco@uci.edu>
2016-10-10 06:13:05 -0700

    fixed login button bug

--- app/src/main/java/com/example/franciscofranco/marvellogin/MainActivity.java
+++ app/src/main/java/com/example/franciscofranco/marvellogin/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.franciscofranco.marvellogin;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
@@ -11,6 +13,9 @@ public class MainActivity extends AppCompatActivity
         implements ConnectivityReceiver.ConnectivityReceiverListener {
 
     private ImageView image;
+    private Button btn;
+
+    // i need to store my buttton or else it will get destroyed
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +23,7 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
 
         image = (ImageView) findViewById(R.id.img);
+        btn = (Button) findViewById(R.id.button1);
 
         checkConnection();
 
@@ -29,6 +35,16 @@ public class MainActivity extends AppCompatActivity
 
         // register connection status listener
         MyApplication.getInstance().setConnectivityListener(this);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
     }
 
     private void checkConnection() {
@@ -43,7 +59,6 @@ public class MainActivity extends AppCompatActivity
         } else {
             displayNetworkError();
         }
-
     }
 
     @Override
@@ -54,25 +69,28 @@ public class MainActivity extends AppCompatActivity
     private void displayLogin() {
         int value = 0;
 
+        btn.setVisibility(View.VISIBLE);
+
         if(getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE){
             value = R.mipmap.login_landscape;
-            Picasso.with(this)
-                    .load(value)
-                    .fit()
-                    .into(image);
+            btn.setY(100);
+
         } else {
             value = R.mipmap.login;
-            Picasso.with(this)
-                    .load(value)
-                    .fit()
-                    .into(image);
+            btn.setY(1000);
+
         }
 
-
+        Picasso.with(this)
+                .load(value)
+                .fit()
+                .into(image);
     }
 
     private void displayNetworkError() {
         int value = 0;
+
+        btn.setVisibility(View.GONE);
 
         if(getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE){
             value = R.mipmap.network_error_landscape;
