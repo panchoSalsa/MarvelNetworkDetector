6eea01a50999d6fbbee9e743f1db0addc1cf1c8c
Franco <frfranco@uci.edu>
2016-10-10 08:41:40 -0700

    successfully retrieved users facebook profile name

--- app/src/main/java/com/example/franciscofranco/marvellogin/HomeActivity.java
+++ app/src/main/java/com/example/franciscofranco/marvellogin/HomeActivity.java
@@ -6,14 +6,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
 import com.facebook.login.LoginManager;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class HomeActivity extends AppCompatActivity {
     private CallbackManager callbackManager;
+    private TextView info;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +30,10 @@ public class HomeActivity extends AppCompatActivity {
         callbackManager = CallbackManager.Factory.create();
 
         setContentView(R.layout.activity_home);
+
+        info = (TextView) findViewById(R.id.info);
+
+        greet();
     }
 
     private void gotoLogin(){
@@ -56,5 +67,39 @@ public class HomeActivity extends AppCompatActivity {
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+    public void greet() {
+        GraphRequest request = GraphRequest.newMeRequest(
+                AccessToken.getCurrentAccessToken(),
+                new GraphRequest.GraphJSONObjectCallback() {
+                    @Override
+                    public void onCompleted(
+                            JSONObject object,
+                            GraphResponse response) {
+                        parseJSON(object);
+                    }
+                });
+
+        Bundle parameters = new Bundle();
+        parameters.putString("fields", "id,name");
+        request.setParameters(parameters);
+        request.executeAsync();
+    }
+
+    public void parseJSON(JSONObject obj) {
+        String name = "";
+
+        try {
+            name = obj.getString("name");
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("Welcome back " + name + "!\n");
+
+        info.setText(sb.toString());
     }
 }
